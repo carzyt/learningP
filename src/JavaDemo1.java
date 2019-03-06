089975cd225904664fe864c0e813e05b184c5b88
zyt <290988625@qq.com>
2019-03-06 22:43:54 +0800

    test

--- src/JavaDemo1.java
+++ src/JavaDemo1.java
@@ -4,5 +4,7 @@
 public class JavaDemo1 {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
     }
 }
