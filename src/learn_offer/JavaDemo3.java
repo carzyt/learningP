dc1e9fda987d52a69f1bc6f03c2b89cb955a6f92
carzyt <290988625@qq.com>
2019-03-07 09:18:54 +0800

    1

---
+++ src/learn_offer/JavaDemo3.java
@@ -0,0 +1,12 @@
+package learn_offer;
+
+/**
+ * Created by Administrator on 2019/3/6.
+ */
+public class JavaDemo3 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
