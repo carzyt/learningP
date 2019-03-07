e2109e948772c64e073e2d10f1b9dc8e2f930d81
carzyt <290988625@qq.com>
2019-03-07 09:32:31 +0800

    1

---
+++ src/learn_sort/JavaDemo5.java
@@ -0,0 +1,12 @@
+package learn_sort;
+
+/**
+ * Created by Administrator on 2019/3/6.
+ */
+public class JavaDemo5 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
