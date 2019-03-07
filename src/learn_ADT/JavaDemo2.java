b63be1c337289e40163088679cab877b78bad2e6
learn <290988625@qq.com>
2019-03-07 08:52:34 +0800

    1

---
+++ src/learn_ADT/JavaDemo2.java
@@ -0,0 +1,12 @@
+package learn_ADT;
+
+/**
+ * Created by Administrator on 2019/3/6.
+ */
+public class JavaDemo2 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
